ce2802c0dda1c41080d7a0b82888e0814fed6d97
marekMak <104523950+marekMak@users.noreply.github.com>
2024-05-12 22:09:07 +0200

    Update index.js
    partners min-h-screen

--- pages/partners/index.js
+++ pages/partners/index.js
@@ -7,7 +7,7 @@ import { fadeIn } from '../../variants';
 
 const Testimonials = () => {
   return (
-    <div className='h-full bg-primary/30 py-32 text-center'>
+    <div className='h-full h-min-screen bg-primary/30 py-32 text-center'>
       <div className='container mx-auto h-full h-min-screen flex flex-col justify-center'>
         {/* title */}
         <motion.h2
