e6b644d7775ba357e6528a67d5fa5abbe47a87c9
marekMak <104523950+marekMak@users.noreply.github.com>
2024-05-21 22:13:30 +0200

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -42,7 +42,7 @@ const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;
   return (
-    <nav className='flex flex-col items-center xl:justify-center gap-y-4 absolute h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
+    <nav className='flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-10 xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
       {/* inner */}
       <div
         className='flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10
