624485d95e4231ce6532951993da082d19e4ae01
marekMak <104523950+marekMak@users.noreply.github.com>
2025-03-23 20:46:38 +0100

    Update index.js

--- pages/asterisk/[id]/index.js
+++ pages/asterisk/[id]/index.js
@@ -38,7 +38,7 @@ import {
           exit='hidden'
           className='flex items-center justify-center'>
           
-          <div dangerouslySetInnerHTML={{__html: data.content.rendered}} className='px-10 w-2/3'></div>
+          <div dangerouslySetInnerHTML={{__html: data.content.rendered}} className='px-10 w-2/3 post-content'></div>
           </motion.div>
       </div>   
     );
