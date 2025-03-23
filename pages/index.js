6ecb0b9c4a268e632afa5adec7bc7c4f58aaf01c
marekMak <104523950+marekMak@users.noreply.github.com>
2025-03-23 20:51:52 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,73 +1,61 @@
 // next image
-import Image from 'next/image';
+import Image from "next/image";
 
 // components
-import ParticlesContainer from '../components/ParticlesContainer';
-import ProjectsBtn from '../components/ProjectsBtn';
+import ParticlesContainer from "../components/ParticlesContainer";
+import ProjectsBtn from "../components/ProjectsBtn";
 
 // framer motion
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 // variants
-import { fadeIn } from '../variants';
+import { fadeIn } from "../variants";
 
 const Home = () => {
   return (
-    <div className='bg-primary/60 min-h-screen'>
+    <div className="bg-primary/60 min-h-screen">
       {/* text */}
-      <div className='w-full min-h-screen bg-gradient-to-r from-primary/10 via-black/30 to-black/10 flex justify-center items-center'>
-        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+      <div className="w-full min-h-screen bg-gradient-to-r from-primary/10 via-black/30 to-black/10 flex justify-center items-center">
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           {/* title */}
           <motion.h1
-            variants={fadeIn('down', 0.2)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
-            className='h1'
+            variants={fadeIn("down", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="h1"
           >
             IB
-            <span className='text-accent'>asterisk</span>.
+            <span className="text-accent">asterisk</span>.
           </motion.h1>
           {/* subtitle */}
           <motion.p
-            variants={fadeIn('down', 0.3)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
-            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-3 xl:mb-6 text-justify font-bold'
+            variants={fadeIn("down", 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-3 xl:mb-6 text-justify font-bold text-xl"
           >
             Ahoj, som Ivan Baroňák,
           </motion.p>
 
           <motion.p
-            variants={fadeIn('up', 0.4)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
-            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 text-justify'
+            variants={fadeIn("up", 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 text-justify"
           >
-           človek závislý na káve, Linuxe a Windowse.<span className='block'>Rád sa venujem aj tvorbe webstránok.</span>O, a mám rád kávu. A Linux.
+            človek závislý na káve, Linuxe a Windowse.
+            <span className="block">Rád sa venujem aj tvorbe webstránok.</span>
+            O, a mám rád kávu. A Linux.
           </motion.p>
-          
-          {/* btn */}
-          {/* <div className='flex justify-center xl:hidden relative'>
-            <ProjectsBtn />
-          </div>
-          <motion.div
-            variants={fadeIn('down', 0.4)}
-            initial='hidden'
-            animate='show'
-            exit='hidden'
-            className='hidden xl:flex'
-          >
-            <ProjectsBtn />
-          </motion.div> */}
         </div>
       </div>
       {/* image */}
-      <div className='w-[1200px] h-full absolute right-0 bottom-0'>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
         {/* bg img */}
-        <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'></div>
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
         {/* particles */}
         <ParticlesContainer />
       </div>
