56bc86317db14a3204c983657a303af6bed0b043
marekMak <104523950+marekMak@users.noreply.github.com>
2025-03-23 20:52:40 +0100

    Update index.js

--- pages/partners/index.js
+++ pages/partners/index.js
@@ -1,30 +1,30 @@
 // components
-import TestimonialSlider from '../../components/TestimonialSlider';
+import TestimonialSlider from "../../components/TestimonialSlider";
 
 // framer motion
-import { motion } from 'framer-motion';
-import { fadeIn } from '../../variants';
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
 
 const Testimonials = () => {
   return (
-    <div className='h-min-screen bg-primary/30 py-32 text-center'>
-      <div className='container mx-auto flex flex-col justify-center'>
+    <div className="h-screen bg-primary/30 pt-32 text-center">
+      <div className="container mx-auto flex flex-col justify-center">
         {/* title */}
         <motion.h2
-          variants={fadeIn('up', 0.2)}
-          initial='hidden'
-          animate='show'
-          exit='hidden'
-          className='h2 mb-8 xl:mb-0'
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
+          className="h2 mb-8 xl:mb-0"
         >
-          Moje <span className='text-accent'>spolupráce</span>.
+          Moje <span className="text-accent">spolupráce</span>.
         </motion.h2>
         {/* slider */}
         <motion.div
-          variants={fadeIn('up', 0.4)}
-          initial='hidden'
-          animate='show'
-          exit='hidden'
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          animate="show"
+          exit="hidden"
         >
           <TestimonialSlider />
         </motion.div>
