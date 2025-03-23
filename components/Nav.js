df96a6610894c79a679b1a83ea6001e06d5f15c3
marekMak <104523950+marekMak@users.noreply.github.com>
2025-03-23 21:07:31 +0100

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -59,8 +59,10 @@ const Nav = () => {
             >
               {/* tooltip */}
               <div className="absolute pr-14 right-0 hidden xl:group-hover:flex">
-                <div className="bg-white relative flex text-primary items-center p-[6px] rounded-[3px]">
-                  <div className="text-[12px] font-semibold">{link.name}</div>
+                <div className="bg-white relative w-24 flex text-primary flex items-center p-[6px] rounded-[3px]">
+                  <div className="text-[12px] font-semibold pl-2">
+                    {link.name}
+                  </div>
                   {/* triangle */}
                   <div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2"></div>
                 </div>
